33c228b4a7582e23a6b4b6981442897950cabe8a
Dulio Denis <dulio.denis@yahoo.com>
2015-01-26 19:28:43 -0500

    Assignment 1 as of the end of Lecture 1

---
+++ assignment-1-calculator/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dulio Denis on 1/26/15.
+//  Copyright (c) 2015 ddApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        println("digit = \(digit)")
+        
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+}
+
---
+++ assignment-1-calculator/README.md
@@ -0,0 +1,9 @@
+# Assignment 1 - Calculator
+This repo contains my first cs193p Winter 2015 assignment from Paul Hegarty's classic [CS 193p iPhone Application Development course](http://web.stanford.edu/class/cs193p/cgi-bin/drupal/) offered by The School of Engineering at Stanford.
+
+![](https://raw.githubusercontent.com/duliodenis/HackingWithSwift/master/art/Calculator.gif)
+
+The first assignment is a Calculator in Swift.
+
+### Support or Contact
+Visit [ddApps.co](http://ddapps.co) to see more.
